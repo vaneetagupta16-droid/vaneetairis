afed53fc1efe4f291de427997bb070d14ecbbbae
VaneetaGupta <vaneetagupta16@gmail.com>
2025-08-29 13:59:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def predict():
 
 
 if __name__ == '__main__':
-    port= int(os.environ.get('PORT',1000))
-    app.run(host='0.0.0.0',port=port
+app.run(debug=False,host="0.0.0.0",port=5000)
+
 
 
